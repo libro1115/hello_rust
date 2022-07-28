b421ca0aeb4b5a153e393b05ec51e3a84194164e
=libro1115 <libro.1115@gmail.com>
2022-07-28 15:55:09 +0900

    乱数の生成

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::io;//入出力ライブラリ
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
     println!("please input number");
+    let secret_number = rand::thread_rng().gen_range(1..101);
     let mut guess = String::new();//letは変数宣言mutは変数が可変であることを示す
     io::stdin()
         .read_line(&mut guess)//mutがないと参照先で不変になる
