e67ca0f5e80e5683869e5c62f2b20d22a163921e
=libro1115 <libro.1115@gmail.com>
2022-07-28 15:46:32 +0900

    入力まで

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;//入出力ライブラリ
+
 fn main() {
-    println!("Hello, rust!");
+    println!("Guess the number");
+    println!("please input number");
+    let mut guess = String::new();//letは変数宣言mutは変数が可変であることを示す
+    io::stdin()
+        .read_line(&mut guess)//mutがないと参照先で不変になる
+        .expect("Failed to read line");//失敗時処理
+
+    println!("you guessd: {}", guess);
 }
