771f23701c9d73c0de18ebe29e74aba5ee631709
=libro1115 <libro.1115@gmail.com>
2022-07-28 16:14:37 +0900

    入力と答えの大・小・正解判定

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,10 @@ fn main() {
 
     println!("you guessd: {}", guess);
 
+    match guess.cmp(&secret_number)
+    {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("Jackpot!"),
+    }
 }
