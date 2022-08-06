7f8222041cb298839b06fae0b055e4ef4cff7f75
=libro1115 <libro.1115@gmail.com>
2022-08-06 20:48:51 +0900

    テスト作成

--- src/Rectangle.rs
+++ src/Rectangle.rs
@@ -1,34 +1,56 @@
-pub trait  Polygon{
-    fn area(&self)->u32;
-    fn corners(&self)->i32;
-    fn can_hold<T:Polygon>(&self, other:&T)->bool;
-    fn width(&self)->u32;
-    fn height(&self)->u32;
+pub trait Polygon {
+    fn area(&self) -> u32;
+    fn corners(&self) -> i32;
+    fn can_hold<T: Polygon>(&self, other: &T) -> bool;
+    fn width(&self) -> u32;
+    fn height(&self) -> u32;
 }
 
-pub struct Rectangle
-{
-   pub width:u32,
-   pub height:u32,
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
 }
-impl Polygon for Rectangle
-{
-    fn area(&self)->u32
-    {
+impl Polygon for Rectangle {
+    fn area(&self) -> u32 {
         self.width * self.height
     }
-    fn corners(&self)->i32 
-    {
+    fn corners(&self) -> i32 {
         2
     }
-    fn can_hold<T :Polygon>(&self, other:&T)->bool
-    {
+    fn can_hold<T: Polygon>(&self, other: &T) -> bool {
         self.width > other.width() && self.height > other.height()
     }
-    fn width(&self)->u32 {
+    fn width(&self) -> u32 {
         self.width
     }
-    fn height(&self)->u32 {
+    fn height(&self) -> u32 {
         self.height
     }
 }
+
+
+#[cfg(test)]
+mod rect_test
+{
+    use super::*;
+    #[test]
+    fn check_rectangle()
+    {
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+        let rect2 = Rectangle {
+            width: 10,
+            height: 40,
+        };
+        let rect3 = Rectangle {
+            width: 60,
+            height: 45,
+        };
+        assert_eq!(rect1.area(), 1500);
+        assert_eq!(rect1.can_hold(&rect2), true);
+        assert_eq!(rect1.can_hold(&rect3), false);
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-mod Rectangle;
+mod rectangle;
 
-use crate::Rectangle::Polygon;
-use crate::Rectangle::Rectangle as Rect;
+use crate::rectangle::Polygon;
+use crate::rectangle::Rectangle as Rect;
 //#[derive(Debug)]
 fn main() {
-    let rect1 = Rect { width: 30, height: 50 };
-    let rect2 = Rect { width: 10, height: 40 };
-    let rect3 = Rect { width: 60, height: 45 };
+    let rect1 = Rect {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rect {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rect {
+        width: 60,
+        height: 45,
+    };
 
     // rect1にrect2ははまり込む？
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    println!("Rect1 Size{}",rect1.area());
-    
+    println!("Rect1 Size{}", rect1.area());
 }
